8f0fbdc1279fb076c688eb35eabd1d78d67d238c
MohamedElaassal <mohamedelaassal42@gmail.com>
2025-03-28 20:47:39 +0000

    add some migrations

---
+++ database/migrations/2025_03_23_143827_create_publications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('utilisateur_id')->constrained('utilisateurs')->onDelete('cascade');
+            $table->string('titre', 255);
+            $table->string('lien')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publications');
+    }
+};
