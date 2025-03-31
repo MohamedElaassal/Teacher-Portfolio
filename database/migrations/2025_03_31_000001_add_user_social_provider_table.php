1fa7625f05c6f954cc1fdf802412dfaa3fcd5752
MohamedElaassal <mohamedelaassal42@gmail.com>
2025-03-31 21:47:07 +0000

    fix some migration issues

--- database/migrations/2025_03_31_000001_add_user_social_provider_table.php
+++ database/migrations/2025_03_31_000001_add_user_social_provider_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('social_provider_user', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('utilisateurs')->onDelete('cascade');
             $table->string('provider_slug'); // maps to providers slug in the devdojo.auth.providers
 
             $table->string('provider_user_id');
---
+++ database/migrations/2025_03_31_000002_update_passwords_field_to_be_nullable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('utilisateurs', function (Blueprint $table) {
+            $table->string('mot_de_passe')->nullable()->change();
+            $table->string('nom')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        // Update records with NULL values to avoid constraint violations
+        DB::table('utilisateurs')->whereNull('nom')->update(['nom' => '']);
+        DB::table('utilisateurs')->whereNull('mot_de_passe')->update(['mot_de_passe' => '']);
+
+        // Change the table structure
+        Schema::table('utilisateurs', function (Blueprint $table) {
+            $table->string('nom')->nullable(false)->change();
+            $table->string('mot_de_passe')->nullable(false)->change();
+        });
+    }
+};
